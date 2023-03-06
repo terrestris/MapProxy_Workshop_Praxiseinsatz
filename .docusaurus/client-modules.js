4f63afbad2a3f3c04c60a06196e840dc6a86a33a
blitza@terrrestris.de <blitza@terrestris.de>
2023-03-06 10:29:03 +0100

    remove tmp folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/hblitza/workspace/MapProxy_Workshop_Praxiseinsatz/node_modules/infima/dist/css/default/default.css'),
-  require('/home/hblitza/workspace/MapProxy_Workshop_Praxiseinsatz/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/hblitza/workspace/MapProxy_Workshop_Praxiseinsatz/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/hblitza/workspace/MapProxy_Workshop_Praxiseinsatz/src/css/custom.css'),
-];
